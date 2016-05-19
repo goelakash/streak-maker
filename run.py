6161cd447f9af8e961cc3d922ff8ed9f8549e6a6
goelakash <goelakash93@gmail.com>
2016-05-19 05:39:24 +0530

    Working only through user input

--- run.py
+++ run.py
@@ -1,28 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env ipython
 
 from builtins import str
 import datetime
-import os
 import pexpect
+import time
 
 commit_message = "Last commit on " + str(datetime.datetime.now().time())
 commits = open("commits.txt","r")
 line  = commits.readline()
+line = line[:-1]
 
 credentials = open("credentials","r")
 username = credentials.readline()
+username = username[:-1]
 password = credentials.readline()
+password = password[:-1]
 
 if line != commit_message:
-
     commits = open("commits.txt","w")
     commits.write(commit_message)
     commits.close()
 
-    os.system("git add .")
-    os.system("git commit -m '"+commit_message+"'")
-    child = pexpect.spawn("git push origin master")
-    child.expect("Username*",async=True)
-    child.sendline(username)
-    child.expect("Password*",async=True)
-    child.sendline(password)
+pexpect.run("git add .")
+pexpect.run("git commit -m '"+commit_message+" amazing'")
+ch = pexpect.spawn("git push origin master")
+time.sleep(2)
+ch.expect('U*',async=True)
+ch.sendline(username)
+time.sleep(2)
+ch.expect('P*',async=True)
+ch.sendline(password)
